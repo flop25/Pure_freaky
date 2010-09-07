a464daad0c4f3a67db10eb1ed37c02e16223e97f
flop25 <flop25@piwigo.org>
2010-09-07 12:18:33 +0000

    version 1.0.0 for pwg 2.1.x
    git-svn-id: http://piwigo.org/svn/extensions/floPure/Pure_freaky@6861 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ themeconf.inc.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Theme Name: Pure_freaky
+Version: 1.0.0
+Description: it's more a concept style than a style. It's Freaky, Man !
+Theme URI: http://fr.piwigo.org/ext/extension_view.php?eid=444
+Author: flop25
+Author URI: http://www.planete-flop.fr/site.php?acc=file
+*/
+
+$themeconf = array(
+  'name'         => 'Pure_freaky',
+  'parent'        => 'Pure_default',
+  'icon_dir'      => 'themes/Pure_default/icon',
+  'mime_icon_dir' => 'themes/Pure_default/icon/mimetypes/',
+);
+
+?>
